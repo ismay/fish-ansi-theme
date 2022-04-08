eb6ab67218e4c8460e85e3c12133b70f80f23808
ismay <7355199+ismay@users.noreply.github.com>
2022-04-08 10:30:43 +0200

    Add pager colors

--- conf.d/ansi-theme.fish
+++ conf.d/ansi-theme.fish
@@ -1,7 +1,8 @@
+# Prompt
 set fish_color_autosuggestion brblack
-set fish_color_cancel --reverse
+set fish_color_cancel red --bold
 set fish_color_command brgreen
-set fish_color_comment brmagenta
+set fish_color_comment brblack --italics
 set fish_color_cwd green
 set fish_color_cwd_root red
 set fish_color_end brmagenta
@@ -9,9 +10,12 @@ set fish_color_error brred
 set fish_color_escape brcyan
 set fish_color_history_current --bold
 set fish_color_host normal
+set fish_color_host_remote normal
+set fish_color_keyword yellow
 set fish_color_match --background=brblue
 set fish_color_normal normal
 set fish_color_operator cyan
+set fish_color_option magenta
 set fish_color_param brblue
 set fish_color_quote yellow
 set fish_color_redirection bryellow
@@ -19,4 +23,15 @@ set fish_color_search_match bryellow --background=brblack
 set fish_color_selection white --bold --background=brblack
 set fish_color_status red
 set fish_color_user brgreen
-set fish_color_valid_path --underline
+set fish_color_valid_path green
+
+# Pager
+set fish_pager_color_progress brblack --italics
+set fish_pager_color_background normal
+set fish_pager_color_prefix yellow
+set fish_pager_color_completion normal
+set fish_pager_color_description brblue --italics
+set fish_pager_color_selected_background --reverse
+set fish_pager_color_selected_prefix yellow --reverse --bold
+set fish_pager_color_selected_completion normal --reverse --bold
+set fish_pager_color_selected_description normal --italics --bold
